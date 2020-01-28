5d9c9617d0f911d7f93e627282f14756e059f367
Chmouel Boudjnah <chmouel@chmouel.com>
2020-01-28 20:33:25 +0100

    Detect path properly
    Signed-off-by: Chmouel Boudjnah <chmouel@chmouel.com>

--- run-operator.sh
+++ run-operator.sh
@@ -2,5 +2,8 @@
 set -eu
 
 [[ -n ${TARGET_NAMESPACE} ]] && args="-n ${TARGET_NAMESPACE}"
+[[ -e ./operator/collectlogs.py ]] && operator_path=./operator/collectlogs.py
+[[ -e /src/collectlogs.py ]] && operator_path=/src/collectlogs.py
 
-exec kopf run ${args} ./operator/collectlogs.py
+
+exec kopf run ${args} ${operator_path}
