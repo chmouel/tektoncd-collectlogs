b22898ece0d978c56ef54585ca7afdf2a8ee9844
Chmouel Boudjnah <chmouel@chmouel.com>
2020-02-07 02:44:41 +0100

    Fix dockerfile
    Signed-off-by: Chmouel Boudjnah <chmouel@chmouel.com>

--- run-operator.sh
+++ run-operator.sh
@@ -5,7 +5,7 @@ TARGET_NAMESPACE=${TARGET_NAMESPACE:-""}
 
 [[ -n ${TARGET_NAMESPACE} ]] && args="-n ${TARGET_NAMESPACE}"
 [[ -e ./operator/collectlogs.py ]] && operator_path=./operator/collectlogs.py
-[[ -e /src/collectlogs.py ]] && operator_path=/src/collectlogs.py
+[[ -e /src/operator/collectlogs.py ]] && operator_path=/src/operator/collectlogs.py
 
 
 exec kopf run ${args} ${operator_path}
