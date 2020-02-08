ef2fcddffacd398db9e9dc5faa735f2942b228b0
Chmouel Boudjnah <chmouel@chmouel.com>
2020-02-08 07:30:50 +0100

    Really need to stop fiddling wiht this file

--- kubernetes/test/populate.sh
+++ kubernetes/test/populate.sh
@@ -6,20 +6,17 @@ NS="foo bar blahblah"
 
 
 for ns in ${NS};do
-	kubectl delete -n ${ns} pr --all || true
-	kubectl delete -n ${ns} tr --all || true
+	kubectl delete -n ${ns} pipelinerun --all || true
+	kubectl delete -n ${ns} taskrun --all || true
+
     kubectl create ns ${ns} 2>/dev/null || true
     
-	for i in pipelinerun taskrrun task ;do
-		kubectl -n ${ns} delete $i --all || true
-	done
-
     kubectl -n ${ns} delete -f pipeline-randomwords.yaml -f pipeline-failmixed.yaml || true
     kubectl -n ${ns} create -f pipeline-randomwords.yaml -f pipeline-failmixed.yaml
 
     kubectl delete pr --all 2>/dev/null || true
     
-	for i in {1..20};do
+	for i in {1..2};do
 		tkn p -n ${ns} start pipeline-failmixed
 		tkn p -n ${ns} start pipeline-randomwords
 	done
